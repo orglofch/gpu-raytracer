224c685186f9d61d8a2b8793eba8f83c82d72410
oglofch <oglfcheski@hotmail.com>
2017-01-08 11:18:23 -0800

    Add plane intersection and multiple lights.

--- GPU-Raytracer/main.cpp
+++ GPU-Raytracer/main.cpp
@@ -27,22 +27,45 @@
 #include <vector>
 
 #include "Utility\algebra.hpp"
+#include "Utility\colour.hpp"
 #include "Utility\gl.hpp"
 #include "Utility\quaternion.hpp"
 
-/** If you change this value, change it in the shaders as well. **/
-const static int MAX_SPHERES = 200;
+/** If you change these values, change it in the shaders as well. **/
+const static int MAX_LIGHTS = 5;
+const static int MAX_SPHERES = 5;
+const static int MAX_PLANES = 5;
+
+struct Light
+{
+	Point3 pos;
+	Colour colour;
+};
 
 struct Sphere
 {
 	Point3 center;
+	Vector3 velocity;
+
 	int radius = 0;
+};
+
+struct Plane
+{
+	Vector3 normal;
+	float d;
 };
 
 struct RaytracerShader : public Shader
 {
+	Uniform light_uniform = -1;
+	Uniform light_count_uniform = -1;
+
 	Uniform sphere_uniform = -1;
 	Uniform sphere_count_uniform = -1;
+
+	Uniform plane_uniform = -1;
+	Uniform plane_count_uniform = -1;
 
 	Uniform origin_uniform = -1;
 	Uniform camera_matrix_uniform = -1;
@@ -54,8 +77,14 @@ struct State
 
 	RaytracerShader shader;
 
+	Light lights[MAX_LIGHTS];
+	int active_lights = 0;
+
 	Sphere spheres[MAX_SPHERES];
 	int active_spheres = 0;
+
+	Plane planes[MAX_PLANES];
+	int active_planes = 0;
 
 	Quaternion rotation;
 };
@@ -63,11 +92,52 @@ struct State
 State state;
 
 void update() {
-	state.rotation *= Quaternion(0.0, 0.003, 0.0, 1.0);
+	for (int i = 0; i < state.active_spheres; ++i) {
+		Sphere &sphere = state.spheres[i];
+		sphere.center += sphere.velocity;
+		if (sphere.center.x - sphere.radius < -200) {
+			sphere.center.x = -200 + sphere.radius;
+			sphere.velocity.x *= -0.99;
+		} else if (sphere.center.x + sphere.radius > 200) {
+			sphere.center.x = 200 - sphere.radius;
+			sphere.velocity.x *= -0.99;
+		}
+		if (sphere.center.y - sphere.radius < -200) {
+			sphere.center.y = -200 + sphere.radius;
+			sphere.velocity.y *= -0.99;
+		} else if (sphere.center.y + sphere.radius > 200) {
+			sphere.center.y = 200 - sphere.radius;
+			sphere.velocity.y *= -0.99;
+		}
+		if (sphere.center.z - sphere.radius < -200) {
+			sphere.center.z = -200 + sphere.radius;
+			sphere.velocity.z *= -0.99;
+		} else if (sphere.center.z + sphere.radius > 200) {
+			sphere.center.z = 200 - sphere.radius;
+			sphere.velocity.z *= -0.99;
+		}
+	}
+	//state.rotation *= Quaternion(0.0, 0.003, 0.0, 1.0);
 }
 
 void render() {
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(state.shader.program);
+
+	// Serialize the lights.
+	GLfloat *light_data = new GLfloat[state.active_lights * 4];
+	for (int i = 0; i < state.active_lights; ++i) {
+		const Light &light = state.lights[i];
+		int index = i * 4;
+		light_data[index + 0] = light.pos.x;
+		light_data[index + 1] = light.pos.y;
+		light_data[index + 2] = light.pos.z;
+		light_data[index + 3] = 0;
+	}
+
+	glUniform4fv(state.shader.light_uniform, state.active_spheres, light_data);
+	glUniform1i(state.shader.light_count_uniform, state.active_lights);
 
 	// Serialize the spheres.
 	GLfloat *sphere_data = new GLfloat[state.active_spheres * 4];
@@ -80,9 +150,22 @@ void render() {
 		sphere_data[index + 3] = sphere.radius;
 	}
 
-	glUseProgram(state.shader.program);
 	glUniform4fv(state.shader.sphere_uniform, state.active_spheres, sphere_data);
 	glUniform1i(state.shader.sphere_count_uniform, state.active_spheres);
+
+	// Serialize the planes.
+	GLfloat *plane_data = new GLfloat[state.active_planes * 4];
+	for (int i = 0; i < state.active_planes; ++i) {
+		const Plane &plane = state.planes[i];
+		int index = i * 4;
+		plane_data[index + 0] = plane.normal.x;
+		plane_data[index + 1] = plane.normal.y;
+		plane_data[index + 2] = plane.normal.z;
+		plane_data[index + 3] = plane.d;
+	}
+
+	glUniform4fv(state.shader.plane_uniform, state.active_planes, plane_data);
+	glUniform1i(state.shader.plane_count_uniform, state.active_planes);
 
 	Point3 eye = state.rotation.matrix() * Point3(0, 0, -600);
 
@@ -166,8 +249,12 @@ void init() {
 	glutSpecialUpFunc(handleReleaseSpecialKey);
 
 	state.shader.program = glLoadShader("raytracer.vert", "raytracer.frag");
+	state.shader.light_uniform = glGetUniform(state.shader, "lights");
+	state.shader.light_count_uniform = glGetUniform(state.shader, "light_count");
 	state.shader.sphere_uniform = glGetUniform(state.shader, "spheres");
 	state.shader.sphere_count_uniform = glGetUniform(state.shader, "sphere_count");
+	state.shader.plane_uniform = glGetUniform(state.shader, "planes");
+	state.shader.plane_count_uniform = glGetUniform(state.shader, "plane_count");
 	state.shader.origin_uniform = glGetUniform(state.shader, "origin");
 	state.shader.camera_matrix_uniform = glGetUniform(state.shader, "camera_matrix");
 }
@@ -178,13 +265,32 @@ void cleanup() {
 int main(int argc, char **argv) {
 	srand((unsigned int)time(NULL));
 
+	state.lights[0].pos = Point3(0, 0, 0);
+	state.lights[1].pos = Point3(0, 0, -600);
+	state.active_lights = 2;
+
 	for (int i = 0; i < MAX_SPHERES; ++i) {
 		state.spheres[i].center.x = Randf(-200, 200);
 		state.spheres[i].center.y = Randf(-200, 200);
 		state.spheres[i].center.z = Randf(-200, 200);
-		state.spheres[i].radius = Randf(5, 50);
+		state.spheres[i].velocity.x = Randf(-3, 3);
+		state.spheres[i].velocity.y = Randf(-3, 3);
+		state.spheres[i].velocity.z = Randf(-3, 3);
+		state.spheres[i].radius = Randf(25, 50);
 	}
 	state.active_spheres = MAX_SPHERES;
+
+	state.planes[0].normal = Vector3(-1, 0, 0);
+	state.planes[0].d = 200;
+	state.planes[1].normal = Vector3(1, 0, 0);
+	state.planes[1].d = 200;
+	state.planes[2].normal = Vector3(0, 0, -1);
+	state.planes[2].d = 200;
+	state.planes[3].normal = Vector3(0, -1, 0);
+	state.planes[3].d = 200;
+	state.planes[4].normal = Vector3(0, 1, 0);
+	state.planes[4].d = 200;
+	state.active_planes = 5;
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
--- GPU-Raytracer/raytracer.frag
+++ GPU-Raytracer/raytracer.frag
@@ -1,31 +1,59 @@
 # version 120
 
 /** The maximum number of metaballs that can be passed to this shader. */
-#define MAX_SPHERES 200
+#define MAX_LIGHTS      5
+#define MAX_SPHERES     5
+#define MAX_PLANES      5
+#define MAX_REFLECTIONS 2
 
-#define INF 1.0 / 0.0
-#define EPSILON 0.01
-#define MAX_REFLECTIONS 1
+#define INF             1.0 / 0.0
+#define EPSILON         0.01
+
+/** Contains for lights, may not be full. */
+uniform vec4 lights[MAX_LIGHTS];
+/** The actual number of lights. */
+uniform int light_count;
 
 /** Container for sphere, may not be full. */
 uniform vec4 spheres[MAX_SPHERES];
-
 /** The actual number of spheres. */
 uniform int sphere_count;
+
+/** Contains for planes, may not be full. */
+uniform vec4 planes[MAX_PLANES];
+/** The actual number of planes. */
+uniform int plane_count;
 
 /** The camera origin. */
 uniform vec3 origin;
 uniform mat4 camera_matrix;
 
 /**
+ * Calculates the intersection with the given plane,
+ * storing it in the passed argument.
+ *
+ * @param ro The origin of the cast ray.
+ * @param dir The direction of the cast ray.
+ * @param index The index of the plane to intersect.
+ * @param t The t value of the intersection in the ray's parametric equation.
+ * @return Whether an intersection was found.
+ */
+ bool findPlaneIntersection(in vec3 ro, in vec3 dir, in int index, out float t) {
+	vec4 plane = planes[index];
+
+    t = -(dot(plane.xyz, ro) + plane.w) / dot(plane.xyz, dir);
+    return t > 0.0;
+ }
+
+/**
  * Calculates the intersections with the given sphere,
- * storing it in passed argument.
+ * storing it in the passed argument.
  *
  * @param ro The origin of the cast ray.
  * @param dir The direction of the cast ray.
  * @param index The index of the sphere to intersect.
- * @param intersections The structures for storing the intersection results.
- * @return The number of intersections found.
+ * @param t The t value of the intersection in the ray's parametric equation.
+ * @return Whether an intersection was found.
  */
 bool findSphereIntersection(in vec3 ro, in vec3 dir, in int index, out float t) {
 	vec3 fc = ro - spheres[index].xyz;
@@ -39,7 +67,7 @@ bool findSphereIntersection(in vec3 ro, in vec3 dir, in int index, out float t) 
 			return false;
 		} else {
 			t = -c / b;
-			return true;
+			return t > 0.0;
 		}
 	} else {
 		float det = b*b - 4.0 * a*c;
@@ -52,7 +80,7 @@ bool findSphereIntersection(in vec3 ro, in vec3 dir, in int index, out float t) 
 			if (q != 0) {
 				t = min(t, c / q);
 			}
-			return true;
+			return t > 0.0;
 		}
 	}
 	return false;
@@ -60,58 +88,54 @@ bool findSphereIntersection(in vec3 ro, in vec3 dir, in int index, out float t) 
 
 bool findIntersection(in vec3 ro, in vec3 rd, out vec3 pos, out vec3 normal) {
 	float min_t = INF;
-	int inter_i = -1;
 	for (int i = 0; i < sphere_count; ++i) {
 		float itr_t;
-		if (findSphereIntersection(ro, rd, i, itr_t)
-				&& itr_t > 0 && itr_t < min_t) {
+		if (findSphereIntersection(ro, rd, i, itr_t) && itr_t < min_t) {
 			min_t = itr_t;
-			inter_i = i;
+			pos = ro + rd * min_t;
+			normal = normalize(pos - spheres[i].xyz);
 		}
 	}
-	if (inter_i == -1) {
-		return false;
+	for (int i = 0; i < plane_count; ++i) {
+		float itr_t;
+		if (findPlaneIntersection(ro, rd, i, itr_t) && itr_t < min_t) {
+			min_t = itr_t;
+			pos = ro + rd * min_t;
+			normal = planes[i].xyz;
+		}
 	}
-
-	pos = ro + rd * min_t;
-	normal = normalize(pos - spheres[inter_i].xyz);
-	return true;
+	return min_t != INF;
 }
 
 vec3 ambient = vec3(0.025, 0.025, 0.05);
 
 vec3 colourForIntersection(in vec3 rd, in vec3 pos, in vec3 normal) {
-	vec3 light_pos = vec3(0, 0, -600);
-	vec3 light_dir = normalize(light_pos - pos);
-
 	vec3 colour = ambient;
+	for (int i = 0; i < light_count; ++i) {
+		vec3 light_pos = lights[i].xyz;
+		vec3 light_dir = normalize(light_pos - pos);
 
-	vec3 l_pos, l_normal;
-	if (!findIntersection(light_pos, -1 * light_dir, l_pos, l_normal)
-			|| distance(light_pos, l_pos) >= distance(light_pos, pos) - EPSILON) {
-		float shininess = 200.0;
+		vec3 l_pos, l_normal;
+		if (!findIntersection(light_pos, -1 * light_dir, l_pos, l_normal)
+				|| distance(light_pos, l_pos) >= distance(light_pos, pos) - EPSILON) {
+			float shininess = 200.0;
 
-		float lambertian = max(dot(light_dir, normal), 0.0);
-		float specular = 0.0;
-		if (lambertian > 0.0) {			
-			vec3 refl_dir = reflect(-light_dir, normal);
-			float spec_angle = max(dot(refl_dir, -rd), 0.0);
-			specular = pow(spec_angle, shininess/4.0);
+			float lambertian = max(dot(light_dir, normal), 0.0);
+			float specular = 0.0;
+			if (lambertian > 0.0) {			
+				vec3 refl_dir = reflect(-light_dir, normal);
+				float spec_angle = max(dot(refl_dir, -rd), 0.0);
+				specular = pow(spec_angle, shininess/4.0);
+			}
+
+			colour += lambertian * vec3(0.25, 0.25, 0.5)
+				+ specular * vec3(1.0, 1.0, 1.0);
 		}
-
-		colour += lambertian * vec3(0.25, 0.25, 0.5)
-			+ specular * vec3(1.0, 1.0, 1.0);
 	}
 	return colour;
 }
 
-void main()
-{
-	vec3 pixel_in_world = (vec4(gl_FragCoord.xy, 0, 1) * camera_matrix).xyz;
-
-	vec3 ro = origin;
-	vec3 rd = normalize(pixel_in_world - origin);
-	
+vec3 castRay(vec3 ro, vec3 rd) {
 	vec3 final_colour = vec3(0.0);
 	float colour_frac = 1.0;
 	for (int i = 0; i < MAX_REFLECTIONS + 1; ++i) {
@@ -128,6 +152,13 @@ void main()
 		ro = pos + normal * EPSILON;
 		rd = normalize(reflect(rd, normal));
 	}
+	return final_colour;
+}
 
-	gl_FragColor = vec4(final_colour, 1);
+void main()
+{
+	vec3 pixel_in_world = (vec4(gl_FragCoord.xy, 0, 1) * camera_matrix).xyz;
+	vec3 rd = normalize(pixel_in_world - origin);
+
+	gl_FragColor = vec4(castRay(origin, rd), 1);
 }
